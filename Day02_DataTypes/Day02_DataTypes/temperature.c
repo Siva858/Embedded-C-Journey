435204c521b3048ab594d9513b0ddfd15ce4f3cf
Siva858 <sivananda858@gmail.com>
2026-01-07 07:48:14 +0530

    Create temperature.c

---
+++ Day02_DataTypes/Day02_DataTypes/temperature.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float temperature;
+    printf("enter your body temperature:");
+    scanf("%f",&temperature);
+    printf("the temperature is %f",temperature);
+    return 0;
+}
