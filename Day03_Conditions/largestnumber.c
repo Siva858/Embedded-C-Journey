a744286c7c131b4a96653164a58fc6fc7e04ac89
Siva858 <sivananda858@gmail.com>
2026-01-09 08:24:18 +0530

    Create largestnumber.c

---
+++ Day03_Conditions/largestnumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter a number");
+    scanf("%d",&a);
+    
+    printf("enter a number");
+    scanf("%d",&b);
+    
+    
+    if(a>b){
+        printf("a is gretaer than b");
+    }else{
+        printf("b is greater than a");
+    }
+    return 0;
+}
